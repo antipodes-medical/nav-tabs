02a5c08fffe5ce73f5f80019f693dad29cbe53be
theobenoit <theo.benoit16@gmail.com>
2022-05-14 14:17:11 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Tabs
+
+## Installation
+
+```bash
+yarn add @antipodes-medical/back-to-top
+```
+
+```javascript
+import '@antipodes-medical/back-to-top';
+```
+
+## Utilisation
+
+```html
+
+<nav-tabs class="tabs-nav">
+    <a href="#tab1" aria-selected="true">Tab 1</a>
+    <a href="#tab2">Tab 2</a>
+    <a href="#tab3">Tab 3</a>
+</nav-tabs>
+<div>
+    <div id="tab1">
+        <p>Lorem ipsum
+            <a href="#">dolor</a>
+            sit amet, consectetur adipisicing elit. Alias delectus, deleniti dicta earum eligendi eos
+            est excepturi facere id ipsa ipsam, iusto laboriosam libero minima nemo optio porro sapiente velit!
+        </p>
+        <p>Ad asperiores aspernatur commodi ducimus, earum hic incidunt iure nam, quasi quia quisquam quos ratione rerum
+            sapiente, sequi sint tenetur totam vero vitae voluptatem? Alias doloribus eum hic quod reiciendis.</p>
+    </div>
+    <div id="tab2" hidden="hidden">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque consectetur, dolor doloribus id illum
+            iure magnam maxime modi molestias necessitatibus odio quae quod repellendus repudiandae sapiente tempora
+            tenetur vitae!</p>
+        <p>Assumenda beatae dolore dolorem ducimus ea eligendi enim et eum exercitationem id in libero minima nam nihil
+            perferendis quam repellat repudiandae rerum saepe sit totam veniam, vitae voluptas voluptatem,
+            voluptates.</p>
+    </div>
+    <div id="tab3" hidden="hidden">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda dignissimos ex facere, in laborum,
+            laudantium minima nam neque nihil nulla pariatur quasi totam vel vitae. Hic odit omnis optio!</p>
+        <p>Ab adipisci amet atque autem beatae dolor dolore, dolores eos error excepturi explicabo illo inventore modi
+            natus non numquam, quidem quod ratione repellat rerum similique soluta suscipit temporibus totam
+            voluptas.</p>
+    </div>
+</div>
+```
--- src/index.js
+++ src/index.js
@@ -1,1 +1,102 @@
-// Do something incredible
+class Tabs extends HTMLElement {
+
+	connectedCallback() {
+		const tabs = Array.from(this.children);
+		const hash = window.location.hash.replace('#', '');
+		let currentTab = tabs[0];
+
+		this.setAttribute('role', 'tablist');
+
+		for (const [i, tab] of tabs.entries()) {
+			const id = tab.getAttribute('href').replace('#', '');
+			const tabPanel = document.getElementById(id);
+
+			if (tab.getAttribute('aria-selected') === 'true' && hash === '') currentTab = tab;
+			if (id === hash) currentTab = tab;
+
+			// Add aria attributes on the tab
+			tab.setAttribute('role', 'tab');
+			tab.setAttribute('aria-selected', 'false');
+			tab.setAttribute('tabindex', '-1');
+			tab.setAttribute('aria-controls', id);
+			tab.setAttribute('id', `tab-${id}`);
+
+			// Add aria attributes on the tab panel
+			tabPanel.setAttribute('role', 'tabpanel');
+			tabPanel.setAttribute('aria-labelledby', `tab-${id}`);
+			tabPanel.setAttribute('hidden', 'hidden');
+			tabPanel.setAttribute('tabindex', '0');
+
+			// Keyboard navigation
+			tab.addEventListener('keyup', e => {
+				let index = null;
+
+				if (e.key === 'ArrowRight') {
+					index = i === tabs.length - 1 ? 0 : i + 1;
+				} else if (e.key === 'ArrowLeft') {
+					index = i === 0 ? tabs.length - 1 : i - 1;
+				} else if (e.key === 'Home') {
+					index = 0;
+				} else if (e.key === 'End') {
+					index = tabs.length - 1;
+				}
+
+				if (index !== null) {
+					this.activate(tabs[index]);
+					tabs[index].focus();
+				}
+			});
+
+			// Mouse navigation
+			tab.addEventListener('click', e => {
+				e.preventDefault();
+				this.activate(tab);
+			});
+		}
+
+		this.activate(currentTab, false);
+		if (currentTab.getAttribute('aria-controls') === hash) {
+			const scrollPadding = 20;
+
+			window.requestAnimationFrame(() => {
+				window.scroll({
+					left: 0,
+					top: currentTab.getBoundingClientRect().top + window.scrollY - scrollPadding,
+					behavior: 'smooth'
+				});
+			});
+		}
+	}
+
+	/**
+   * Activate a tab
+   *
+   * @param {Element} tab
+   * @param {boolean} changeHash
+   */
+	activate(tab, changeHash = true) {
+		const currentTab = this.querySelector('[aria-selected="true"]');
+		if (currentTab !== null) {
+			const tabPanel = document.getElementById(currentTab.getAttribute('aria-controls'));
+
+			currentTab.setAttribute('aria-selected', 'false');
+			currentTab.setAttribute('tabindex', '-1');
+			tabPanel.setAttribute('hidden', 'hidden');
+		}
+		const id = tab.getAttribute('aria-controls');
+		const tabPanel = document.getElementById(id);
+
+		tab.setAttribute('aria-selected', 'true');
+		tab.setAttribute('tabindex', '0');
+
+		tabPanel.removeAttribute('hidden');
+		if (changeHash) {
+			window.history.replaceState({}, '', `#${id}`);
+		}
+	}
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	customElements.define('nav-tabs', Tabs);
+});
